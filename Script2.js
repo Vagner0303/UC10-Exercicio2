4cb7cb704ac5bff78f2ca85469c4360ff2babf1e
Vagner Melo <vagnermelopires10@gmail.com>
2026-02-20 14:23:38 -0300

    Update Script2.js

--- Script2.js
+++ Script2.js
@@ -1,1 +1,13 @@
-console.log{'Seja Bem-Vindo'}
+let NF = 6 //Nota Final
+let NR = 8 // Nota de Recuperação
+let T1 = 7 // Nota Trabalho 1
+let T2 = 9 // Nota Trabalho 2
+let T3 = 5 // Nota Trabalho 3
+
+// Logica de Aprovação
+
+let trabalhosAprovados = (T1 > 6) + (T2 > 6) + (T3 > 6) //Soma dos Trabalhos Aprovados
+
+let aprovado = (NF > 7) || (NR >= 8 && trabalhosAprovados >= 2);
+
+console.log{aprovado}
